f6b4a03af34f97b59dbfe6dcae0e84f7ea9e1718
Ug0510 <uditgg0510@gmail.com>
2024-03-25 15:59:03 +0530

    adding

---
+++ index.js
@@ -0,0 +1,15 @@
+// Import the Express.js module
+const express = require('express');
+
+// Create an instance of Express
+const app = express();
+
+// Define a route that responds with "Hello, World!" when accessed
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server and listen on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
